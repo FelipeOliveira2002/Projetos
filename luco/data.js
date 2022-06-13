17eb686f445eb28486d2e71429210947f3593305
FelipeOliveiraSuwawa <felipeoliveira2002pires@gmail.com>
2022-06-13 15:54:51 -0300

    f

---
+++ luco/data.js
@@ -0,0 +1,30 @@
+const articles = [
+    {
+        id:1,
+        title:'the best of the lands',
+        date: new Date(2020,9,19),
+        length:11,
+        snippet:`なになにプリン食べたいの早く席から立って車の中で充満するわよ`,
+    },
+    {
+        id:2,
+        title:'落ちれば落ちるほど化合物嗅ぎます',
+        date: new Date(2018,5,2),
+        length:5,
+        snippet:`やっぱり恐怖じゃないただ大幅な机だった`,
+    },
+    {
+        id:3,
+        title:'あたしってさ昨日ってさカエルってさキスしてしまったってさ',
+        date: new Date(2001,4,2),
+        length:5,
+        snippet: `その勤務を務める根気があったらそれでもできん`,
+    },
+    {
+        id:4,
+        title:'牛乳飲まなくてええやんもう乳製品いっぱいあるから',
+        date: new Date(2009,3,5),
+        length:5,
+        snippet:`最高で最高じゃなくてもテレビを橋の下に引っ掛けるわ`
+    },
+];
