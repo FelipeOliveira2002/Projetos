ce72a47ac13b81032d925fcb74926c40ac36ebe7
FelipeOliveiraSuwawa <felipeoliveira2002pires@gmail.com>
2022-06-14 22:02:59 -0300

    j

---
+++ TemSim/asami-app/src/componets/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import{CloudUploadIcon,DatabaseIcon,PaperAirplaneIcon,ServerIcon,}from '@heroicons/react/solid'
+import bgImg from '../assets/cyber-bg.png'
+
+ const Hero = () => {
+  return (
+    <div className='w-full h-screen bg-zinc-200 flex flex-col justify-between'>
+        
+
+    </div>
+  )
+}
+
+export default Hero
