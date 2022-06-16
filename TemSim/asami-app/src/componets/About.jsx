efebe347ddcc2d39f08bf10154a3897f34185303
FelipeOliveiraSuwawa <felipeoliveira2002pires@gmail.com>
2022-06-15 22:31:33 -0300

    a

--- TemSim/asami-app/src/componets/About.jsx
+++ TemSim/asami-app/src/componets/About.jsx
@@ -9,17 +9,17 @@ const About = () => {
                 <p className='text-3xl py-6 text-gray-500'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita quia, minus neque molestiae illo sit fugiat quam, voluptates commodi, perspiciatis ea</p>
             </div>
             <div className='grid md:grid-cols-3 gap-1 px-2 text-center'>
-                <div className='border py-8 rounded-xl'>
-                    <p>100%</p>
-                    <p>Completion</p>
+                <div className='border py-8 rounded-xl shadow-xl'>
+                    <p className='text-6xl font-bold text-indigo-600'>100%</p>
+                    <p className='text-gray-400 mt-2'>Completion</p>
                 </div>
-                <div className='border py-8 rounded-xl'>
-                    <p>24/7</p>
-                    <p>Delivery</p>
+                <div className='border py-8 rounded-xl shadow-xl'>
+                    <p className='text-6xl font-bold text-indigo-600'>24/7</p>
+                    <p className='text-gray-400 mt-2'>Delivery</p>
                 </div>
-                <div className='border py-8 rounded-xl'>
-                    <p>100k</p>
-                    <p>Transation</p>
+                <div className='border py-8 rounded-xl shadow-xl'>
+                    <p className='text-6xl font-bold text-indigo-600'>100k</p>
+                    <p className='text-gray-400 mt-2'>Transactions</p>
                 </div>
             </div>
         </div>
