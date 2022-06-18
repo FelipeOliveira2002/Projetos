00a6cc5af0bbebb6e5fbadb30b1dfd2a94df9084
FelipeOliveiraSuwawa <felipeoliveira2002pires@gmail.com>
2022-06-18 20:02:55 -0300

    Update Support.jsx

--- TemSim/asami-app/src/componets/Support.jsx
+++ TemSim/asami-app/src/componets/Support.jsx
@@ -15,15 +15,35 @@ const Support = () => {
                 <h3 className='text-5xl font-bold py-6 text-center'>Finding the right team</h3>
                 <p className='py-4 text-3xl text-slate-300'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium, magni mollitia! Exercitationem inventore soluta sequi fugiat velit modi porro dolorum laborum, saepe repudiandae</p>
             </div>
-            <div>
-                <div>
-                    <div>
-                        <PhoneIcon />
-                        <h3>Sales</h3>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit voluptate rerum officiis perferendis</p>
+            <div className='grid grid-cols-1 lg:grid-cols-3 relative gap-x-8 gap-y-16 px-4 pt-12 sm:pt-20 text-black'>
+                <div className='bg-white rounded-xl shadow-2xl'>
+                    <div className='p-8'>
+                        <PhoneIcon className='w-16 p-4 bg-indigo-600 text-white rounded-lg mt-[-4rem]' />
+                        <h3 className='font-bold text-2xl my-6'>Sales</h3>
+                        <p className='text-gray-600 text-xl'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit voluptate rerum officiis perferendis</p>
                     </div>
-                    <div>
-                        <p>Contact Us <ArrowSmRightIcon/></p>
+                    <div className='bg-slate-100 pl-8 py-4'>
+                        <p className='flex items-center text-indigo-600'>Contact Us <ArrowSmRightIcon className='w-5 ml-2'/></p>
+                    </div>
+                </div>
+                <div className='bg-white rounded-xl shadow-2xl'>
+                    <div className='p-8'>
+                        <SupportIcon className='w-16 p-4 bg-indigo-600 text-white rounded-lg mt-[-4rem]' />
+                        <h3 className='font-bold text-2xl my-6'>Technical Support</h3>
+                        <p className='text-gray-600 text-xl'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit voluptate rerum officiis perferendis</p>
+                    </div>
+                    <div className='bg-slate-100 pl-8 py-4'>
+                        <p className='flex items-center text-indigo-600'>Contact Us <ArrowSmRightIcon className='w-5 ml-2'/></p>
+                    </div>
+                </div>
+                <div className='bg-white rounded-xl shadow-2xl'>
+                    <div className='p-8'>
+                        <ChipIcon className='w-16 p-4 bg-indigo-600 text-white rounded-lg mt-[-4rem]' />
+                        <h3 className='font-bold text-2xl my-6'>Media Inquiries</h3>
+                        <p className='text-gray-600 text-xl'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit voluptate rerum officiis perferendis</p>
+                    </div>
+                    <div className='bg-slate-100 pl-8 py-4'>
+                        <p className='flex items-center text-indigo-600'>Contact Us <ArrowSmRightIcon className='w-5 ml-2'/></p>
                     </div>
                 </div>
             </div>
